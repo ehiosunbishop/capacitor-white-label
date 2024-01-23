01e399bf7ff1cd607706d83a4ee4e7d9a2ce4358
Ehiosun <ehiosunbishop@gmail.com>
2024-01-23 10:31:17 +0100

    worked on trapeze yaml

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const generateAssets = argv.generateAssets || false;
 const appFlowChannel = argv.appFlowChannel || null;
 const hostname = argv.hostname || appId;
 
+
 // Check if appId is provided
 if (!appId) {
      console.error('Error: App ID is required. Use the --appId flag to specify the App ID.');
@@ -69,27 +70,56 @@ const yamlFileName = 'trapeze-config.yaml';
 const yamlFilePath = path.join(projectRoot, yamlFileName);
 
 try {
-     const yamlContent = yaml.dump({
-          platforms: {
-               android: {
-                    appName: appName,
-                    packageName: appId,
-                    versionName: versionNumber,
-                    versionCode: buildNumber
-               },
-               ios: {
-                    targets: {
-                         App: {
-                              version: versionNumber,
-                              buildNumber: buildNumber,
-                              bundleId: appId,
-                              displayName: appName,
-                              productName: appName
-                         }
-                    }
-               }
-          }
-     });
+     const yamlContent = `
+platforms:
+  android:
+    appName: ${appName}
+    packageName: ${appId}
+    versionName: ${versionNumber}
+    versionCode: ${buildNumber}
+    manifest:
+     - file: AndroidManifest.xml
+       target: 'manifest'
+       attrs:
+         package: ${appId}
+    gradle:
+     - file: app/build.gradle
+       target:
+         android:
+           namespace:
+       replace:
+         namespace: ${appId}
+    xml:
+     - resFile: values/strings.xml
+       target: resources/string[@name="app_name"]
+       replace: |
+         <string name="app_name">${appName}</string>
+
+     - resFile: values/strings.xml
+       target: resources/string[@name="title_activity_main"]
+       replace: |
+         <string name="title_activity_main">${appName}</string>
+
+     - resFile: values/strings.xml
+       target: resources/string[@name="package_name"]
+       replace: |
+         <string name="package_name">${appId}</string>
+
+     - resFile: values/strings.xml
+       target: resources/string[@name="custom_url_scheme"]
+       replace: |
+         <string name="package_name">${appId}</string>
+  ios:
+    targets:
+      App:
+        version: ${versionNumber}
+        buildNumber: ${buildNumber}
+        bundleId: ${appId}
+        displayName: ${appName}
+        productName: ${appName}
+        buildSettings:
+          INFOPLIST_KEY_CFBundleDisplayName: ${appName}
+     `;
 
      // Write the YAML content to the file
      fs.writeFileSync(yamlFilePath, yamlContent, 'utf-8');
